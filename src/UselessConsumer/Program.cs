4f8e43fa253ea06141e9b75558aee26ac4a8fbdf
Dávid Hornyák <davidhornyak92@gmail.com>
2021-02-14 17:07:24 +0100

    Add package reference and configure NuGet sources for the repo

--- src/UselessConsumer/Program.cs
+++ src/UselessConsumer/Program.cs
@@ -1,12 +1,27 @@
-﻿using System;
-
-namespace UselessConsumer
+﻿namespace UselessConsumer
 {
-    class Program
+    using System;
+
+    using UselessPackage;
+
+    public static class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
+            var uselessDependency = new UselessImplementation();
+            var uselessObject = new UselessClass();
+            var uselessResult = uselessObject.UselessMethod(uselessDependency, 0);
+
             Console.WriteLine("Hello World!");
+            Console.WriteLine($"Result of this useless computation: {uselessResult}");
+        }
+
+        private class UselessImplementation : IUselessInterface
+        {
+            public int Call(int input)
+            {
+                return input;
+            }
         }
     }
 }
