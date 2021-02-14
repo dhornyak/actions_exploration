c49a82fa75aeb5e9c2271f06c71819793b1b1384
Dávid Hornyák <davidhornyak92@gmail.com>
2021-02-14 16:05:02 +0100

    Add console project

---
+++ src/UselessConsumer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UselessConsumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
